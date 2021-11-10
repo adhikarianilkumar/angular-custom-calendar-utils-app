02afcca7806363d43ad63e5b6b1ac7e587688a1c
anilkumar-adhikari <anilkumar.adhikari01@gmail.com>
2021-11-10 17:38:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bg7rbf-whbaa1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bg7rbf-whbaa1)
---
+++ demo/module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter, CalendarUtils } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoComponent } from './component';
+import { CustomCalendarUtils } from './custom-calendar-utils';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }, {
+      utils: {
+        provide: CalendarUtils,
+        useClass: CustomCalendarUtils
+      }
+    })
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent]
+})
+export class DemoModule {}
